fefb9054ea9a856b0fab66db9e53bbe2fbc747b8
Alang_laptop <Alang@laptop.com>
2024-04-25 14:27:10 +0800

    修改viewset

--- output/views.py
+++ output/views.py
@@ -18,15 +18,15 @@ class LastDayOutPutViewSet(ModelViewSetPlus):
 
     def get_time_queryset(self):
         today = datetime.now().date()
-        # today = date(2024, 4, 14)
+        # today = date(2024, 4, 22)
         weekday = today.weekday()
         if weekday == 0:
-            start_date = today - timedelta(days=1)
+            start_date = today - timedelta(days=6)
         else:
-            start_date = today - timedelta(days=weekday)
+            start_date = today - timedelta(days=weekday-1)
 
         before_today_this_week = super().get_queryset().filter(
-            time__range=[start_date, today]
+            time__range=[start_date, today + timedelta(days=1)]
         )
         return before_today_this_week
 
@@ -44,15 +44,15 @@ class RealOEEDayViewSet(ModelViewSetPlus):
 
     def get_time_queryset(self):
         today = datetime.now().date()
-        # today = date(2024, 4, 14)
+        # today = date(2024, 4, 22)
         weekday = today.weekday()
         if weekday == 0:
-            start_date = today - timedelta(days=1)
+            start_date = today - timedelta(days=6)
         else:
-            start_date = today - timedelta(days=weekday)
+            start_date = today - timedelta(days=weekday-1)
 
         before_today_this_week = super().get_queryset().filter(
-            time__range=[start_date, today]
+            time__range=[start_date, today + timedelta(days=1)]
         )
         return before_today_this_week
 
