006f698062c72a20ea05e98347d9dec826269bd9
Alang_laptop <Alang@laptop.com>
2024-05-07 10:23:41 +0800

    添加功能

--- dan_view/urls.py
+++ dan_view/urls.py
@@ -18,14 +18,14 @@ Including another URLconf
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from output.views import LastDayOutPutViewSet, RealOEEDayViewSet
+from output.views import LastDayOutPutViewSet, RealOEEDayViewSet, EVR1ViewSet, EVR2ViewSet
 from public.views import UserViewSet
 
 router = SimpleRouter()
 router.register(r"user", UserViewSet, basename="user")
 router.register(r"last_day_output", LastDayOutPutViewSet, basename="last_day_output")
 router.register(r"real_oee_day", RealOEEDayViewSet, basename="real_oee_day")
-router.register(r"evr1", RealOEEDayViewSet, basename="evr1")
-router.register(r"evr2", RealOEEDayViewSet, basename="evr2")
+router.register(r"evr1", EVR1ViewSet, basename="evr1")
+router.register(r"evr2", EVR2ViewSet, basename="evr2")
 urlpatterns = [
               ] + router.urls
